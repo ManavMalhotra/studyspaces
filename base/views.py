6c395a19d5e3e68996c99ad255127ff9a06276b2
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2023-07-25 23:59:00 +0530

    models

--- base/views.py
+++ base/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
+from .models import Room
 
-room_list = [
-    {'id': 1, 'name': 'Room 1'},
-    {'id': 2, 'name': 'Room 2'},
-    {'id': 3, 'name': 'Room 3'}
-]
+# room_list = [
+#     {'id': 1, 'name': 'Room 1'},
+#     {'id': 2, 'name': 'Room 2'},
+#     {'id': 3, 'name': 'Room 3'}
+# ]
+
 
 def home(request):
-    return render(request, 'base/home.html', {'rooms': room_list})
+    room_list = Room.objects.all()
+    context = room_list
+    return render(request, 'base/home.html', {'rooms': context})
 
-def room(request,pk):
-    room = None
-    for i in room_list:
-        if i['id'] == int(pk):
-            room = i
-            break
-    return render(request, 'base/room.html', {'room': room})
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'id': room.id, 'name': room.name}
+    return render(request, 'base/room.html', {'room': context})
 
+def createRoom(request):
+    return render(request, 'base/room_form.html')
