ec7b95a9647447c9d6c64ce68c194dadff4c372b
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2023-07-27 19:45:38 +0530

    added conversations

--- base/views.py
+++ base/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib import messages
 
@@ -27,8 +27,22 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'id': room.id, 'name': room.name}
-    return render(request, 'base/room.html', {'room': context})
+    room_messages = room.message_set.all().order_by('-created')
+    room_participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    
+
+    context = {'room' : room, 'room_messages': room_messages, 'room_participants': room_participants}
+    return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
 def createRoom(request):
@@ -71,6 +85,19 @@ def deleteRoom(request, pk):
         return redirect('/')
 
     return render(request, 'base/delete.html', {"obj": room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)   
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {"obj": message})
 
 def loginView(request):
     page = 'login'
